fe450c6b8174032d721b79b88f4aae01963c7a27
Dmitriy Minin <dbminin@yandex.ru>
2022-07-02 13:23:34 +0300

    4 ПР.Вторая редакция

--- scripts/index.js
+++ scripts/index.js
@@ -7,21 +7,24 @@ let professionProfile = document.querySelector('.profile__profession');
 let fullNameForm = document.querySelector('.popup__input_form_name');
 let professionForm = document.querySelector('.popup__input_form_profession');
 
-fullNameForm.value = fullNameProfile.textContent;
-professionForm.value = professionProfile.textContent;
-
-let togglePopupOpened = function() {
-  popupContainer.classList.toggle('popup_opened');
+let addPopupOpened = function() {
+  popupContainer.classList.add('popup_opened');
+  fullNameForm.value = fullNameProfile.textContent;
+  professionForm.value = professionProfile.textContent;
 };
+
+let removePopupOpened = function() {
+  popupContainer.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   fullNameProfile.textContent = fullNameForm.value;
   professionProfile.textContent = professionForm.value;
-  popupContainer.classList.toggle('popup_opened');
+  popupContainer.classList.remove('popup_opened');
 }
 
-addButtonPopup.addEventListener('click', togglePopupOpened);
-closeButtonPopup.addEventListener('click', togglePopupOpened);
+addButtonPopup.addEventListener('click', addPopupOpened);
+closeButtonPopup.addEventListener('click', removePopupOpened);
 
 form.addEventListener('submit', formSubmitHandler); 
